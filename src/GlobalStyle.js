7156a2abaa609cd094f0863aec80158cbf957ef6
youhane <jason.sebastian154@gmail.com>
2021-11-21 11:37:31 +0700

    ok

---
+++ src/GlobalStyle.js
@@ -0,0 +1,10 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+ *{
+        font-family: Poppins;
+        font-style: normal;
+        font-weight: 500;
+        transition: all .2s;
+    }
+`
